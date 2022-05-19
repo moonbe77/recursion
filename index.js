4fcbc563072c5297e034279f1a05b69fdfa6136b
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2022-05-19 18:12:11 +1000

    fix alternate render

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const button = document.getElementById("add");
 var list = [
 	{
 		name: "wave 1",
-		value: 100,
+		value: 100000000,
 	},
 	{
 		name: "wave 2",
@@ -25,15 +25,15 @@ var list = [
 	},
 ];
 
-list.sort((a, b) => {
-	return a.name < b.name ? -1 : 1;
-});
+list.sort((a, b) => a.name - a.name);
 
 function renderList(values) {
+	values.sort((a, b) => a.name - b.name);
 	let render = values
 		.map((item, index) => {
 			return `<div class='item'>
-			${item.name}<span class='delete' id="${index}">x</span>
+			<div class="item-value" id=${index}>${item.name}</div>
+			<span class='delete' id="${index}">x</span>
 			</div>`;
 		})
 		.join("");
@@ -42,19 +42,34 @@ function renderList(values) {
 	document.querySelectorAll(".delete").forEach((element) => {
 		element.addEventListener("click", deleteItem);
 	});
+	document.querySelectorAll(".item-value").forEach((el) => {
+		el.addEventListener("click", changeName);
+	});
 }
 renderList(list);
 
 function insertMissing(index, value) {
 	list.splice(index, 0, value);
-	list.sort((a, b) => {
-		return a.name < b.name ? -1 : 1;
-	});
+	// list.sort((a, b) => {
+	// 	return a.name < b.name ? -1 : 1;
+	// });
+	renderList(list);
+}
+
+function renameItem(index, value) {
+	list[index] = { ...list[index], name: value };
+	console.log(list);
 	renderList(list);
 }
 
 function getListMissing(val, prevIndex) {
+	if (val === 0) {
+		return getListMissing(1, 0);
+	}
 	let index = list.indexOf(val);
+	if (index === -1 && list.length === 0) {
+		return { index: 0, val: val };
+	}
 	// find the value on the list
 	const find = list.find((item, index) => {
 		return item.name === `wave ${val}`;
@@ -62,7 +77,7 @@ function getListMissing(val, prevIndex) {
 
 	if (val > list.length) {
 		console.log("Value is greater than list length");
-		return { prevIndex, val: val + 1 };
+		return { prevIndex, val: val };
 	}
 
 	if (find !== undefined) {
@@ -81,10 +96,18 @@ function addMissing() {
 }
 
 function deleteItem(ev) {
-	console.log(ev.target.id);
 	const index = ev.target.id;
 	list.splice(index, 1);
 	renderList(list);
+}
+
+function changeName(ev) {
+	const index = ev.target.id;
+	// const newName = prompt("enter new name");
+	ev.target.onFocus((ev.target.contentEditable = true));
+	console.log(onFocus);
+	//get liST item AND REPLACE IT WITH THE NEW NAME
+	// renameItem(index, newName);
 }
 
 button.addEventListener("click", addMissing);
